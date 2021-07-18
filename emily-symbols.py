f67a7e076022608dd0e08df1b4e32ce6aeeebaf2
JorWat25 <81863525+JorWat25@users.noreply.github.com>
2021-07-18 01:39:51 +0100

    Update emily-symbols.py

--- emily-symbols.py
+++ emily-symbols.py
@@ -19,7 +19,7 @@ symbols = {
         "FRPBG" : ["{#Page_Up}", "{#Home}", "{#End}", "{#Page_Down}"],
         "FRBG"  : ["{#AudioPlay}", "{#AudioPrev}", "{#AudioNext}", "{#AudioMute}"],
         ""      : ["", "{*!}", "{*?}", "{#Space}"],
-        "FL"    : ["{-|}", "{*>}", "{>}", "{*-|}"],
+        "FL"    : ["{*-|}", "{*<}", "{<}", "{*>}"],
 
         # typable symbols
         "FR"     : ["!", "¬", "↦", "¡"],
